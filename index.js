9d164ece6111d6dcd28c3613479f74eee0cee68d
Vpp2000 <48797063+Vpp2000@users.noreply.github.com>
2020-11-23 21:09:52 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-const obtener_cursos = require('./routes');
-app.use('/', obtener_cursos);
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
